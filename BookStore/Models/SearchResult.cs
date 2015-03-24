05a826aa89623d1ea65751912a4cfafbc9f3b49c
Roman <koljadar@gmail.com>
2015-03-25 00:07:22 +0200

    with SearchResult

---
+++ BookStore/Models/SearchResult.cs
@@ -0,0 +1,57 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class SearchResult
+    {
+        public string Title { get; set; }
+        public string htmlTitle { get; set; }
+        public string link { get; set; }
+        public static string GetInnerText(string url)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(getRequest(url));
+            HtmlNode c = doc.DocumentNode.SelectSingleNode("//p[@itemprop='about']");
+            return c == null ? " " : c.InnerText;
+        }
+        public static string getRequest(string url)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.AllowAutoRedirect = false;//Запрещаем автоматический редирект
+            using (var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                using (var stream = httpWebResponse.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+        public static IList<SearchResult> getSearch(string searchText, string cfg = "")
+        {
+            string key = "AIzaSyBzcXSZrtK15FFCX8v_Ob-Hcxnc-cVHc-Y";
+            string cx = "015577388163479462430:16-o3xadmg4";
+            string google = "https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx + "&q=" + searchText + "&alt=json" + cfg;
+            JObject googleSearch = JObject.Parse(getRequest(google));
+            List<JToken> results = googleSearch["items"].Children().ToList();
+            IList<SearchResult> searchResults = new List<SearchResult>();
+            foreach (JToken result in results)
+            {
+                SearchResult searchResult = JsonConvert.DeserializeObject<SearchResult>(result.ToString());
+                searchResults.Add(searchResult);
+            }
+            return searchResults;
+        }
+
+    }
+}
